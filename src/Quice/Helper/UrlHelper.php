4f873ed85b63cb3f0920bd3b31b8c24a60b3f148
sunseesiu <sunseesiu@gmail.com>
2012-01-02 16:48:05 +0800

    remove module and action support

--- src/Quice/Helper/UrlHelper.php
+++ src/Quice/Helper/UrlHelper.php
@@ -16,18 +16,16 @@ class UrlHelper
     public $request = null;
     public $rewrite = false;
 
-    public function href($module = null, $action = null, $query = array())
+    public function href($query = array())
     {
         $url = $this->request->getBaseUrl() . '/';
 
-        $query = array_merge(array('module' => $module, 'action' => $action), $query);
-
         $seprator = '?';
 
-        foreach ($query as $name => $value) {
+        foreach ((array)$query as $name => $value) {
             if ($value) {
                 $url .= $seprator . $name . '=' . urlencode($value);
-                $seprator = '&amp;';
+                $seprator = '&';
             }
         }
 
